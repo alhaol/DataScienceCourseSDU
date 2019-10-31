4472572881f7c29f1d0d4d220f522b15a157a190
Jakob Hviid <jakobhviid1982@gmail.com>
2019-10-31 11:48:32 +0100

    moved files to fit with course and added spark

---
+++ example.py
@@ -0,0 +1,13 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().set(
+    'spark.driver.host', '127.0.0.1')
+sc = SparkContext(master='local', appName='myAppName', conf=conf)
+
+files = "hdfs://10.112.254.50:9000/user/hadoop/inputs"
+txtFiles = sc.wholeTextFiles(files, 20)
+words_in_files = txtFiles.map(lambda s: s[1].split())
+all_word = txtFiles.flatMap(lambda s: s[1].split())
+word_map = all_word.map(lambda s: (s, 1))
+word_reduce = word_map.reduceByKey(lambda s, t: s+t)
+print(word_reduce.sortBy(lambda s: s[1]).collect())
