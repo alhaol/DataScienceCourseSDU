80e82240b182315162b1addd8562d50a18c697e4
Jakob Hviid <jakobhviid1982@gmail.com>
2019-11-16 13:23:29 +0100

    added flume files

--- flumeExampleImage/run.cmd
+++ flumeExampleImage/run.cmd
@@ -1,3 +1,3 @@
 docker build . -t flume:latest 
 
-docker run --rm -it --ip 172.200.0.245 --hostname flume --network hadoop flume:latest 
+docker run --rm -it --ip 172.200.0.245 --hostname flume -p 44444:44444 --network hadoop flume:latest 
