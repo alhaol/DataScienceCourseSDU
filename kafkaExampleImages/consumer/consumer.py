85bd67a69b4ae0312a29f4c151bae58be0668856
Jakob Hviid <jakobhviid1982@gmail.com>
2019-12-01 13:55:25 +0100

    added iteration number

--- kafkaExampleImages/consumer/consumer.py
+++ kafkaExampleImages/consumer/consumer.py
@@ -1,7 +1,4 @@
 from kc import KafkaClient
-import threading
-from time import sleep
-import json
 
 """
  kafka client, python example
@@ -12,10 +9,13 @@ kClient.setup_env(['kafka1:9092', 'kafka2:9092', 'kafka3:9092'])
 
 kClient.subscribe_to_env('c1', 'group1', "testtopic")
 
+
 class ResponseHandler:
 
     def handle(self, message):
-        print("Received record from %s. Content: %s. At Time: %s"%(message.topic, message.value, message.timestamp))
-        
-rh = ResponseHandler()     
+        print("Received record from %s. Content: %s. At Time: %s" %
+              (message.topic, message.value, message.timestamp))
+
+
+rh = ResponseHandler()
 kClient.consume('c1', rh)
--- kafkaExampleImages/producer/producer.py
+++ kafkaExampleImages/producer/producer.py
@@ -1,17 +1,24 @@
 from kc import KafkaClient
-import threading
 from time import sleep
-import json
 
 """
  kafka client, python example
 """
-
+print("start")
 kClient = KafkaClient()
 kClient.setup_env(['kafka1:9092', 'kafka2:9092', 'kafka3:9092'])
+print("setup ok")
 
-data = {"I": "have", "network": "problems"}
+
+d = 0
+print("variable initialized")
+
 
 while True:
+    data = {"prop A": "value A", "prop B": "value B", "iteration": str(d)}
     kClient.produce_to_env("testtopic", data)
+
     print(data)
+    d += 1
+    if d > 100000:
+        break
